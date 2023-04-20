a37bacd5ec9c7c021e1d0b057a33dcdb60aafa5a
Jong Yang <nint317@gmail.com>
2023-04-20 10:14:40 -0600

    Adding initial Student class in the domain layer

---
+++ WebApi/Domain/Students/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Students
+{
+    public class Student
+    {
+        public string Id { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string ContactNumber { get; set; } = String.Empty;
+        public string EmailAddress { get; set; } = String.Empty;
+
+        public Student() { }
+
+        public Student(string id, string firstName, string lastName, string contactNumber, string emailAddress)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            ContactNumber = contactNumber;
+            EmailAddress = emailAddress;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {FirstName} {LastName} {ContactNumber} {EmailAddress}";
+        }
+
+
+
+    }
+}
